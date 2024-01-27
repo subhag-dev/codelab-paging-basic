59763e23605ea39353134c1786b1d56125eb89b7
subhag-dev <subhag.dev@gmail.com>
2024-01-27 23:32:46 +0530

    Add load state

--- app/src/main/java/com/example/android/codelabs/paging/data/ArticlePagingSource.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/ArticlePagingSource.kt
@@ -2,10 +2,13 @@ package com.example.android.codelabs.paging.data
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
+import kotlinx.coroutines.delay
 import java.time.LocalDateTime
 import kotlin.math.max
 
+private const val LOAD_DELAY_MILLIS = 3_000L
 private val firstArticleCreatedTime = LocalDateTime.now()
+
 const val STARTING_KEY = 0
 
 class ArticlePagingSource : PagingSource<Int, Article>() {
@@ -21,6 +24,7 @@ class ArticlePagingSource : PagingSource<Int, Article>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
         val start = params.key ?: STARTING_KEY
         val range = start.until(start + params.loadSize)
+        if (start != STARTING_KEY) delay(LOAD_DELAY_MILLIS)
         return LoadResult.Page(
             data = range.map { number ->
                 Article(
